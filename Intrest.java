812b0b0e2274bf95c405192ad9b87ad64ebe8e33
koushiky <113330038+koushiky@users.noreply.github.com>
2023-02-08 09:59:51 +0530

    Create Intrest.java

---
+++ Intrest.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class INTREST
+{ 
+ public static void main(String[]args)
+ {
+  Scanner sc=new Scanner(System.in);
+  float p=sc.nextFloat();
+  float R=sc.nextFloat();
+  float T=sc.nextFloat();
+  float SI=(P*R*T)/100;
+  System.out.println("Simple interst="+SI);
+ }
+}
